b3505a046702cb0c0ebe69fe41d8e5a429819869
Gage53211 <gagemather@gmail.com>
2024-12-07 13:56:21 -0700

    finished black box test

--- tests/black_box_test.py
+++ tests/black_box_test.py
@@ -3,85 +3,70 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 
 class ReviewTest(unittest.TestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome()  # Web driver swapped for Chrome
         self.browser.get('http://127.0.0.1:5000/signup')
+        self.browser.maximize_window()
         
     def tearDown(self):
         self.browser.quit()
 
-    def test_signup(self): 
-        userName = self.browser.find_element(By.ID, 'username')
-        userName.send_keys('USER1')
-        
-        # Check if the username is already taken
-        if self.browser.find_elements(By.TAG_NAME, 'div')[6].text == "That username is taken. Please choose a different one.":
-            self.browser.quit()  # Skip the rest of the test
-
-        email = self.browser.find_element(By.ID, 'email')
-        email.send_keys('test@test.com')
-
-        password = self.browser.find_element(By.ID, 'password')
-        password.send_keys('123123123123')
-
-        passwordConfirm = self.browser.find_element(By.ID, 'confirm_password')
-        passwordConfirm.send_keys('123123123123')
-
-        submitButton = self.browser.find_elements(By.TAG_NAME, 'button')[2]
-        submitButton.click()
-
-        page = self.browser.current_url
-        self.assertEqual('http://127.0.0.1:5000/signup', page)
 
     def test_new_review(self):  # Movie ID: tt13623148
         
-        #login 
+        #loggin into admin account
         
         self.browser.get('http://127.0.0.1:5000/login')
         
         userName = self.browser.find_element(By.ID, 'username')
-        userName.send_keys('USER1')
+        userName.send_keys('admin')
 
         password = self.browser.find_element(By.ID, 'password')
-        password.send_keys('123123123123')
+        password.send_keys('password')
 
         submitButton = self.browser.find_elements(By.TAG_NAME, 'button')[2]
         submitButton.click()
 
-        p = self.browser.find_element(By.TAG_NAME, 'p').text
-        self.assertEqual("Welcome to the main page, USER1!", p)
+        h2 = self.browser.find_elements(By.TAG_NAME, 'h2')[0].text
+        self.assertEqual("Recommended Movies", h2)
         
         
         #------------------------------------------------------------------------------
 
-        #check for review
+        #create / delete review
         
-        self.browser.get('http://127.0.0.1:5000/random')
-
-        reviewButton = self.browser.find_element(By.CLASS_NAME, 'btn btn-primary')
+        self.browser.get('http://127.0.0.1:5000/media/movie/tt25392008')
+        
+        reviewButton = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "review_button")))
         reviewButton.click()
-        
-        star = WebDriverWait(self.browser, 10).until(
-        EC.element_to_be_clickable((By.TAG_NAME, 'span'))
-        )
-        ActionChains(self.browser).move_to_element(star).click().perform()
+
+        starElement = WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.ID, 'star5')))
+        starElement.click()
 
         comment = self.browser.find_element(By.ID, 'comment')
         comment.send_keys('test')
 
-        submitButton = self.browser.find_elements(By.TAG_NAME, 'button')[9]
+        submitButton = self.browser.find_element(By.ID, 'submit_review')
         submitButton.click()
 
         page = self.browser.current_url
-        self.assertEqual('http://127.0.0.1:5000/media/movie/tt22060390', page)
+        self.assertEqual('http://127.0.0.1:5000/media/movie/tt25392008', page)
 
-        p = self.browser.find_element(By.ID, 'user_comment').text
-        self.assertEqual('test', p)
-  
+        reviewUsername = WebDriverWait(self.browser, 15).until(EC.visibility_of_element_located((By.ID, 'user_comment')))
+        self.assertEqual('test', reviewUsername.text)
+
+        deleteButton = self.browser.find_elements(By.TAG_NAME, 'button')[5]
+        deleteButton.send_keys('\ue007')
+
+        confirmDeleteButton = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "confirm_delete")))
+        confirmDeleteButton.click()
+        
+        confirmationMessage = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'no_reviews')))
+        self.assertEqual(confirmationMessage.text, "Be The First To Review This Movie!")
+
 
 if __name__ == '__main__':
     unittest.main()
